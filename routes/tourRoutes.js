1722f5b2dcf6d303cd26432eb1f03cd95147d038
Rolie Conde <onefivezero999@gmail.com>
2023-06-07 07:16:42 +0800

    Added more data to the Tour Schema

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  createTour,
-  getTours,
-  getTour,
-  updateTour,
-  deleteTour,
-} = require("../controllers/toursController");
+const { createTour, getTours, getTour, updateTour, deleteTour } = require("../controllers/toursController");
 
 //endppoints
 router.route("/").get(getTours).post(createTour);
