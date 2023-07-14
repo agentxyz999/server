eef89f4499e8af39e453d4b8bf82e122792f11e1
Rolie Conde <onefivezero999@gmail.com>
2023-07-14 16:36:07 +0800

    Create aggregation pipeline for Tour Stats

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -124,3 +124,48 @@ exports.deleteAllTour = async (req, res) => {
     });
   }
 };
+
+/////////////////////////
+//AGGREGATION PIPELINES//
+/////////////////////////
+
+//get tour stats
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          // _id: null,
+          // _id: "$ratingsAverage",
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: "EASY" } },
+      // },
+    ]);
+    res.status(200).json({
+      status: "Success",
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,10 +9,15 @@ const {
   deleteTour,
   deleteAllTour,
   topFiveBest,
+  getTourStats,
 } = require("../controllers/toursController");
 
 //endppoints
+
 router.route("/top-5-best").get(topFiveBest, getTours); //get top 5 best tours based on ratings average
+
+//this aggregation route (getTourStats) needs to be put here before the ("/:id") to prevent an error
+router.route("/stats").get(getTourStats);
 
 router.route("/").get(getTours).post(createTour);
 
